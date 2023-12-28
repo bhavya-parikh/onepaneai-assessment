787b91512d07991add7c0b7d741f40c711d095d9
Bhavya-Parikh <12102080701016@mbit.edu.in>
2023-12-28 17:24:33 +0530

    added incidents to chat body

--- src/components/ChatComponent.js
+++ src/components/ChatComponent.js
@@ -3,6 +3,7 @@ import HeaderComponent from "./HeaderComponent";
 import SenderDetailsComponent from "./SenderDetailsComponent";
 import { messages } from "../constants";
 import MessageContentComponent from "./MessageContentComponent";
+import IncidentComponent from "./IncidentComponent";
 
 function ChatComponent() {
   return (
@@ -19,6 +20,21 @@ function ChatComponent() {
             <br />
             <br />
             <MessageContentComponent message={message} />
+            {message.incidents
+              ? message.incidents.map((incident) => {
+                  return (
+                    <>
+                      <IncidentComponent
+                        key={incident.id}
+                        incidentType={incident.incidentType}
+                        incidentMessage={incident.incidentMessage}
+                        incidentPlatform={incident.incidentPlatform}
+                        incidentTime={incident.incidentTime}
+                      />
+                    </>
+                  );
+                })
+              : ""}
           </>
         );
       })}
